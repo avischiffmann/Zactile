9e84b18eec43cbd92f758fcd1d3e849d454baaa6
Avi Schiffmann <avischiffmann@gmail.com>
2020-01-19 15:43:07 -0800

    axios requests

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.render("index");
+});
+
+app.post('/login', (req, res) => {
+  console.log('request:', req.query.firstName + req.query.lastName);
 })
 
 app.listen(port, () => console.log(`Zactile listening on port ${port}!`));
