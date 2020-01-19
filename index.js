1a07ee181b7d02fcf752787f935767dab2466cf4
Avi Schiffmann <avischiffmann@gmail.com>
2020-01-19 14:20:59 -0800

    Inital setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.render("index");
+})
+
+app.listen(port, () => console.log(`Zactile listening on port ${port}!`));
