138c07db4ecb53d6d17de994c0a910e27e7c22db
Avi Schiffmann <avischiffmann@gmail.com>
2020-01-19 16:08:54 -0800

    v1

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ app.get('/', function (req, res) {
     res.render("index");
 });
 
-app.post('/login', (req, res) => {
-  console.log('request:', req.query.firstName + req.query.lastName);
+app.post('/submit', (req, res) => {
+  console.log('title:', req.query.title);
+  console.log('subtitle:', req.query.description);
+  console.log('President:', req.query.President);
+  console.log('vicePresident:', req.query.vicePresident);
+  console.log('Secretary:', req.query.Secretary);
+  console.log('Treasurer:', req.query.Treasurer);
 })
 
 app.listen(port, () => console.log(`Zactile listening on port ${port}!`));
